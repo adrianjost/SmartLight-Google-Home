fe75c4866733365964bdcf086613411650ccbefd
adrianjost <22987140+adrianjost@users.noreply.github.com>
2020-01-09 22:49:41 +0100

    fix function names

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 
 // OAuth Server
-exports["OAuth2-UsersCreate"] = require("./functions/oauth2/user-sync.function.js").CREATE;
-exports["OAuth2-UsersDelete"] = require("./functions/oauth2/user-sync.function.js").DELETE;
+exports["OAuth2UsersCreate"] = require("./functions/oauth2/user-sync.function.js").CREATE;
+exports["OAuth2UsersDelete"] = require("./functions/oauth2/user-sync.function.js").DELETE;
 exports["OAuth2"] = require("./functions/oauth2/server.function.js");
 
 // Home Graph API
