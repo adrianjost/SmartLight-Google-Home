862236407927d3426d9bc12bc1004394ada5d03f
adrianjost <22987140+adrianjost@users.noreply.github.com>
2020-01-10 08:19:17 +0100

    export functions directly

--- functions/oauth2/server.function.js
+++ functions/oauth2/server.function.js
@@ -1,34 +1,27 @@
 // OAuth Server - https://github.com/organicinternet/oauth2-firebase
 
+// Before Deploy Set:
+// firebase functions:config:set crypto.auth_token_secret_key_32=<YOUR_GENERATED_RANDOM_32_CHAR_STRING>
+// firebase functions:config:set project.api_key=<YOUR_WEB_API_KEY>
+
 const functions = require("firebase-functions");
-const app = require("restana")();
 const {
-	authorize,
+  authorize,
 	Configuration,
 	googleAccountAuthentication,
 	token,
 } = require("oauth2-firebase");
 
-// Before Deploy Set:
-// firebase functions:config:set crypto.auth_token_secret_key_32=<YOUR_GENERATED_RANDOM_32_CHAR_STRING>
-// firebase functions:config:set project.api_key=<YOUR_WEB_API_KEY>
-
 Configuration.init({
-	crypto_auth_token_secret_key_32: functions.config().crypto
-		.auth_token_secret_key_32,
+  crypto_auth_token_secret_key_32: functions.config().crypto
+  .auth_token_secret_key_32,
 	project_api_key: functions.config().project.api_key,
 });
 
-
-app.use(require("../middleware/cors"));
-
-app.get("/token", token());
-app.get("/authorize", authorize());
-app.get("/authentication", googleAccountAuthentication());
-
-module.exports = functions
-	.runWith({
-		timeoutSeconds: 30,
-		memory: "512MB",
-	})
-	.https.onRequest(app.callback());
+module.exports = {
+	"OAuth2UsersCreate": require("./user-sync.function.js").CREATE,
+	"OAuth2UsersDelete": require("./user-sync.function.js").DELETE,
+	"OAuth2/token": token(),
+	"OAuth2/authorize": authorize(),
+	"OAuth2/authentication": googleAccountAuthentication(),
+}
--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 
 
 // OAuth Server
-exports["OAuth2UsersCreate"] = require("./functions/oauth2/user-sync.function.js").CREATE;
-exports["OAuth2UsersDelete"] = require("./functions/oauth2/user-sync.function.js").DELETE;
-exports["OAuth2"] = require("./functions/oauth2/server.function.js");
+Object.assign(exports, require("./functions/oauth2/server.function.js"))
 
 // Home Graph API
 exports["GoogleHome"] = require("./functions/index.function.js");
