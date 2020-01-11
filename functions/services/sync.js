788156c8bef43d16e335eff3f9e814503e2ea3ea
adrianjost <22987140+adrianjost@users.noreply.github.com>
2020-01-11 11:05:01 +0100

    add more fields to sync response

--- functions/services/sync.js
+++ functions/services/sync.js
@@ -3,46 +3,46 @@ const pkg = require("../../package.json");
 /*
 req.body:
 {
-  "requestId": "ff36a3cc-ec34-11e6-b1a0-64510650abcf",
-  "inputs": [{
-    "intent": "action.devices.SYNC"
-  }]
+	"requestId": "ff36a3cc-ec34-11e6-b1a0-64510650abcf",
+	"inputs": [{
+		"intent": "action.devices.SYNC"
+	}]
 }
 
 response:
 {
-  "requestId": "ff36a3cc-ec34-11e6-b1a0-64510650abcf",
-  "payload": {
-    "agentUserId": "1836.15267389",
-    "devices": [{
-      "id": "123",
-      "type": "action.devices.types.LIGHT",
-      "traits": [
-        "action.devices.traits.OnOff",
-        "action.devices.traits.ColorSetting"
-      ],
-      "name": {
-        "defaultNames": ["Acme Co. bulb A19 On/Off only"],
-        "name": "lamp1",
-        "nicknames": ["reading lamp"]
-      },
-      "attributes": {
-          "colorModel": "rgb",
-          "colorTemperatureRange": {
-            "temperatureMinK": 2000,
-            "temperatureMaxK": 9000
-          },
-          "commandOnlyColorSetting": false
-        },
-      "willReportState": true,
-      "deviceInfo": {
-        "manufacturer": "Acme Co",
-        "model": "hg11",
-        "hwVersion": "1.2",
-        "swVersion": "5.4"
-      }
-    }]
-  }
+	"requestId": "ff36a3cc-ec34-11e6-b1a0-64510650abcf",
+	"payload": {
+		"agentUserId": "1836.15267389",
+		"devices": [{
+			"id": "123",
+			"type": "action.devices.types.LIGHT",
+			"traits": [
+				"action.devices.traits.OnOff",
+				"action.devices.traits.ColorSetting"
+			],
+			"name": {
+				"defaultNames": ["Acme Co. bulb A19 On/Off only"],
+				"name": "lamp1",
+				"nicknames": ["reading lamp"]
+			},
+			"attributes": {
+					"colorModel": "rgb",
+					"colorTemperatureRange": {
+						"temperatureMinK": 2000,
+						"temperatureMaxK": 9000
+					},
+					"commandOnlyColorSetting": false
+				},
+			"willReportState": true,
+			"deviceInfo": {
+				"manufacturer": "Acme Co",
+				"model": "hg11",
+				"hwVersion": "1.2",
+				"swVersion": "5.4"
+			}
+		}]
+	}
 }
 */
 
@@ -68,33 +68,33 @@ const getUnits = async (userid) => {
 /*
 unit:
 {
-  "state": {
-    "gradient": false,
-    "color": "#ff00ff"
-  },
-  "name": "Bett",
-  "id": "someId",
-  "tags": ["a Tag"],
-  "created_at": {
-    "_seconds": 1578148014,
-    "_nanoseconds": 796000000
-  },
-  "updated_at": {
-    "_seconds": 1578148482,
-    "_nanoseconds": 521000000
-  },
-  "ip": "192.168.1.233",
-  "channelMap": {
-    "b": 1,
-    "r": 2,
-    "g": 3
-  },
-  "updated_by": "userId",
-  "type": "LAMP", // or GROUP
-  "icon": "some-icon",
-  "lamptype": "RGB", // or WWCW
-  "created_by": "userId",
-  "hostname": "some-hostname"
+	"state": {
+		"gradient": false,
+		"color": "#ff00ff"
+	},
+	"name": "Bett",
+	"id": "someId",
+	"tags": ["a Tag"],
+	"created_at": {
+		"_seconds": 1578148014,
+		"_nanoseconds": 796000000
+	},
+	"updated_at": {
+		"_seconds": 1578148482,
+		"_nanoseconds": 521000000
+	},
+	"ip": "192.168.1.233",
+	"channelMap": {
+		"b": 1,
+		"r": 2,
+		"g": 3
+	},
+	"updated_by": "userId",
+	"type": "LAMP", // or GROUP
+	"icon": "some-icon",
+	"lamptype": "RGB", // or WWCW
+	"created_by": "userId",
+	"hostname": "some-hostname"
 }
 */
 const getGroupInfo = (unit) => {
@@ -104,7 +104,11 @@ const getLampInfo = (unit) => {
 	const getTraits = (lamptype) => {
 		// TODO [#2]: differentiate between RGB and WWCW lamps, currenlty only RGB is implemented
 		// TODO [#3]: implement more traits https://developers.google.com/assistant/smarthome/traits
-		return ["action.devices.traits.OnOff"];
+		return [
+			// "action.devices.traits.OnOff",
+			// "action.devices.traits.Brightness",
+			"action.devices.traits.ColorSetting",
+		];
 	};
 	return {
 		id: unit.id,
@@ -118,11 +122,15 @@ const getLampInfo = (unit) => {
 		attributes: {
 			// TODO [#4]: implement colorTemperatureRange for WWCW lamps https://developers.google.com/assistant/smarthome/traits/colorsetting
 			colorModel: "rgb",
+			// commandOnlyOnOff: true,
+			// commandOnlyBrightness: true,
 			commandOnlyColorSetting: true,
 		},
-		willReportState: false,
+		willReportState: true,
 		deviceInfo: {
 			manufacturer: "DIY",
+			model: "Modular PCB",
+			hwVersion: "3.1",
 			swVersion: pkg.version,
 		},
 	};
