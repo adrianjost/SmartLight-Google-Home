7d4c79940ee7464c9e3a3aa3b61573afbfe0b835
adrianjost <22987140+adrianjost@users.noreply.github.com>
2020-01-11 18:54:40 +0100

    rename color helper

--- functions/utils/color.js
+++ functions/utils/color.js
@@ -1,7 +1,7 @@
 const hexToSpectrumRgb = (hexColor) => parseInt(hexColor.slice(1), 16);
-const SpectrumRgbToHex = (spectrumRgb) => `#${spectrumRgb.toString(16)}`;
+const spectrumRgbToHex = (spectrumRgb) => `#${spectrumRgb.toString(16)}`;
 
 module.exports = {
 	hexToSpectrumRgb,
-	SpectrumRgbToHex,
+	spectrumRgbToHex,
 };
