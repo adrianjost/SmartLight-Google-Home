d96c5a6cd8b1e8cafc4f8249b0ea2749822674f1
adrianjost <22987140+adrianjost@users.noreply.github.com>
2020-01-11 22:07:42 +0100

    fix short color strings

--- functions/utils/color.js
+++ functions/utils/color.js
@@ -1,5 +1,6 @@
 const hexToSpectrumRgb = (hexColor) => parseInt(hexColor.slice(1), 16);
-const spectrumRgbToHex = (spectrumRgb) => `#${spectrumRgb.toString(16)}`;
+const spectrumRgbToHex = (spectrumRgb) =>
+	`#${spectrumRgb.toString(16).padStart(6, "0")}`;
 
 module.exports = {
 	hexToSpectrumRgb,
