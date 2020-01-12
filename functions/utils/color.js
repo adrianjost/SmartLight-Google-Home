6b758a44eaba323e2322845c6bb4fcc1fc86d2fa
adrianjost <22987140+adrianjost@users.noreply.github.com>
2020-01-12 17:39:26 +0100

    switched from hsl to hsv for better human understanding
    https://psychology.wikia.org/wiki/HSL_and_HSV

--- functions/utils/color.js
+++ functions/utils/color.js
@@ -8,14 +8,15 @@ const spectrumRgbToHex = (spectrumRgb) =>
  * @param  {string} hexColor 7 digit color string (including `#`)
  * @return {number} luminance of the color - Number between 0 and 100
  */
-const getLuminance = (hexColor) => Color(hexColor).hsl().color[2];
+const getLuminance = (hexColor) => Color(hexColor).hsv().color[2];
+
 /**
  * @param  {string} hexColor 7 digit color string (including `#`)
  * @param  {number} brightness brightness level between 0 and 100 (inclusive)
  * @return new hex color string
  */
 const setLuminance = (hexColor, brightness) => {
-	const hsl = Color(hexColor).hsl();
+	const hsl = Color(hexColor).hsv();
 	hsl.color[2] = brightness;
 	return hsl.hex();
 };
